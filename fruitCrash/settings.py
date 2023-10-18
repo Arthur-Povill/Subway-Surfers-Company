6cb1f852ccc9c3ee63058d30d10c255d504888a0
theus0197 <141360388+theus0197@users.noreply.github.com>
2023-10-18 16:46:59 -0300

    add cors in middle

--- fruitCrash/settings.py
+++ fruitCrash/settings.py
@@ -88,6 +88,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'fruitCrash.urls'
