2f509b721f1edd7725a76701b4f4ec6576fb7fe2
admarketchampion <admarketchampion@gmail.com>
2023-10-30 13:31:55 -0300

    enabled permited db

--- fruitCrash/settings.py
+++ fruitCrash/settings.py
@@ -92,14 +92,14 @@ WSGI_APPLICATION = 'fruitCrash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'railway',
@@ -108,7 +108,7 @@ DATABASES = {
        'HOST': 'containers-us-west-164.railway.app',
        'PORT': '7498',
    }
-}'''
+}
 
 
 # Password validation
